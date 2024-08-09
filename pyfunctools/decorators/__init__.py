d7979f8557bbc5a1573c12f5e9cad994a45a53bd
Natanael dos Santos Feitosa <natansantosapps@gmail.com>
2024-08-09 18:27:47 -0300

    feat: add timeout_decorator

--- pyfunctools/decorators/__init__.py
+++ pyfunctools/decorators/__init__.py
@@ -5,6 +5,7 @@
 from .async_decorator import async_decorator
 from .retry_decorator import retry_decorator
 from .threaded_decorator import threaded_decorator
+from .timeout_decorator import timeout_decorator
 from .timing_decorator import timing_decorator
 
-__all__ = ('async_decorator', 'retry_decorator', 'threaded_decorator', 'timing_decorator',)
+__all__ = ('async_decorator', 'retry_decorator', 'threaded_decorator', 'timeout_decorator', 'timing_decorator',)
